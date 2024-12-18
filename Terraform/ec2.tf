9e49384e77fc8abb2c17c9b79385b0bb61384b5d
Oliver Cutting <oliver@olivercutting.com>
2024-12-18 21:56:50 +0000

    add nginx and flask hello world
    Signed-off-by: Oliver Cutting <oliver@olivercutting.com>

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -3,4 +3,5 @@ resource "aws_instance" "web" {
   instance_type               = "t2.micro"
   associate_public_ip_address = false
   monitoring                  = true
+  user_data                   = "./userdata.bash"
 }
